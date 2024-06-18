60f9889516ae553f906707cf3d532e29cd153786
manas2307 <134101104+manas2307@users.noreply.github.com>
2024-06-19 00:40:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0]; //array containing ms,s,m,h all initialised to zero
+let timeRef = document.querySelector(".timer-display"); //access timer display div
+let int = null;
+
+document.getElementById("start-timer").addEventListener("click", () => { //when click on start button this arrow function will get executed
+    if(int !== null) {
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer, 10);
+});
+
+document.getElementById("pause-timer").addEventListener("click", () => {
+    clearInterval(int);
+});
+
+document.getElementById("reset-timer").addEventListener("click", () => {
+    clearInterval(int);
+    [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+    timeRef.innerHTML = "00 : 00 : 00 : 000 ";
+}); 
+
+function displayTimer() {
+    milliseconds += 10;
+    if(milliseconds == 1000) {
+        milliseconds = 0;
+        seconds++;
+        if(seconds == 60) {
+            seconds = 0;
+            minutes++;
+            if(minutes == 60) {
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = 
+        milliseconds < 10
+        ? "00" + milliseconds
+        : milliseconds < 100
+        ? "0" + milliseconds
+        : milliseconds;
+
+    timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+
+}
